2c4623b8c93eb04169249e463a1506c73253762d
m0hitt17 <mohitdubey1704@gmail.com>
2025-10-03 16:53:30 +0530

    Update Cars.jsx

--- client/src/Pages/Cars.jsx
+++ client/src/Pages/Cars.jsx
@@ -101,7 +101,7 @@ const Cars = () => {
             <motion.div
             initial={{opacity:0,y:20}}
         animate={{opacity:1,y:0}}
-        transition={{duration:0.4,delay:0.1*index,duration:0.4}}
+        transition={{duration:0.4,delay:0.1*index}}
              key={index}>
               <CarCard car={car} />
             </motion.div>
